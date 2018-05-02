6b71103115b1a700bb36bebda9d243f861d4692c
Cosy <staycosy@163.com>
2018-05-02 22:59:08 +0800

    add file

---
+++ doc/ClassDepends.sql
@@ -0,0 +1,4 @@
+INSERT INTO `ClassDepends`(`AT_INDX`, `CLASS_NAME`, `DEPEND_CLASS`, `AUTHOR`, `TIMESTAMP`) VALUES (1, 'testDemo', 'testDemoA', 'CSOY', '2018-04-29');
+INSERT INTO `ClassDepends`(`AT_INDX`, `CLASS_NAME`, `DEPEND_CLASS`, `AUTHOR`, `TIMESTAMP`) VALUES (2, 'testDemoA', 'testDemoB', 'COSY', '2018-04-29');
+INSERT INTO `ClassDepends`(`AT_INDX`, `CLASS_NAME`, `DEPEND_CLASS`, `AUTHOR`, `TIMESTAMP`) VALUES (3, 'testDemoB', 'testDemoC', 'COSY', '2018-05-02');
+INSERT INTO `ClassDepends`(`AT_INDX`, `CLASS_NAME`, `DEPEND_CLASS`, `AUTHOR`, `TIMESTAMP`) VALUES (4, 'testDemoC', 'testDemoD', 'COSY', '2018-05-02');
---
+++ doc/ClassInvokers.sql
@@ -0,0 +1,2 @@
+INSERT INTO `ClassInvokers`(`IV_Index`, `CLASS_NAME`, `AUTHOR`, `TIME`) VALUES (1, 'testDemo', 'cosy', '2018-04-29');
+INSERT INTO `ClassInvokers`(`IV_Index`, `CLASS_NAME`, `AUTHOR`, `TIME`) VALUES (2, 'testDemoB', 'cosy', '2018-05-02');
